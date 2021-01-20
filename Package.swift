f77d4d3c88e2839e5f460316633cd4f0d9773c93
Antoine Palazzolo <antoinepalazzolo@users.noreply.github.com>
2021-01-20 17:27:03 +0100

    test

--- Package.swift
+++ Package.swift
@@ -1,18 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
-#if canImport(Compression)
 let targets: [Target] = [
     .target(name: "ZIPFoundation"),
     .testTarget(name: "ZIPFoundationTests", dependencies: ["ZIPFoundation"])
 ]
-#else
-let targets: [Target] = [
-    .systemLibrary(name: "CZLib", pkgConfig: "zlib", providers: [.brew(["zlib"]), .apt(["zlib"])]),
-    .target(name: "ZIPFoundation", dependencies: ["CZLib"], cSettings: [.define("_GNU_SOURCE", to: "1")]),
-    .testTarget(name: "ZIPFoundationTests", dependencies: ["ZIPFoundation"])
-]
-#endif
 
 let package = Package(
     name: "ZIPFoundation",
@@ -22,6 +14,5 @@ let package = Package(
     products: [
         .library(name: "ZIPFoundation", targets: ["ZIPFoundation"])
     ],
-    targets: targets,
-    swiftLanguageVersions: [.v4, .v4_2, .v5]
+    targets: targets
 )
